4e9f823bfe8511b74a8d89eae42cf5b34dcbbd89
thais <thaismferreira42@gmail.com>
2025-06-25 20:59:14 -0300

    alterando criação do usuário

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,8 +1,7 @@
 const Usuarios = require("../models/Usuario");
-const cloudinary = require('../cloudinary/cloudinary');
+const cloudinary = require("../cloudinary/cloudinary");
 
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
 
 const usuarioController = {
   create: async (req, res) => {
@@ -10,32 +9,56 @@ const usuarioController = {
       const { nome, apelido, nascimento, email, senha } = req.body;
       const { foto, capa } = req.files;
 
-      // configurando hash de senha
-      const salt = await bcrypt.genSalt(12);
-      const hash = await bcrypt.hash(senha, salt);
+      // Função para normalizar e validar o apelido
+      const normalizeApelido = (apelido) => {
+        const limpo = apelido.replace(/[^a-zA-Z0-9]/g, "");
+        return `@${limpo.toLowerCase()}`;
+      };
 
-      // validando se usuário e apelido já existem
+      const isApelidoValido = (apelido) => /^@[a-z0-9]+$/.test(apelido);
+
+      const apelidoNormalizado = normalizeApelido(apelido);
+
+      if (!isApelidoValido(apelidoNormalizado)) {
+        return res.status(400).json({
+          message:
+            "Formato de apelido inválido. Use apenas letras e números, começando com @",
+        });
+      }
+
+      // Verificação de existência de email e apelido (case-insensitive)
       const usuarioExiste = await Usuarios.findOne({ email });
       const apelidoExiste = await Usuarios.findOne({
-        apelido: { $regex: `^${apelido}$`, $options: 'i' }
+        apelido: { $regex: `^${apelidoNormalizado}$`, $options: "i" },
       });
 
       if (usuarioExiste) {
-        return res.status(401).json({ message: "O email inserido está em uso, por gentileza utilize outro." });
+        return res.status(401).json({
+          message: "O email inserido está em uso, por gentileza utilize outro.",
+        });
       }
 
       if (apelidoExiste) {
-        return res.status(401).json({ message: "O apelido inserido já está em uso, por gentileza utilize outro." });
+        return res.status(401).json({
+          message:
+            "O apelido inserido já está em uso, por gentileza utilize outro.",
+        });
       }
+
+      // Hash da senha
+      const salt = await bcrypt.genSalt(12);
+      const hash = await bcrypt.hash(senha, salt);
 
       // Upload da foto para o Cloudinary
       let foto_src;
       if (foto) {
         const result = await new Promise((resolve, reject) => {
-          cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
-            if (error) reject(error);
-            else resolve(result);
-          }).end(foto[0].buffer); // Acessa o primeiro arquivo 'foto'
+          cloudinary.uploader
+            .upload_stream({ resource_type: "image" }, (error, result) => {
+              if (error) reject(error);
+              else resolve(result);
+            })
+            .end(foto[0].buffer); // Acessa o primeiro arquivo 'foto'
         });
         foto_src = result.secure_url;
       }
@@ -44,18 +67,20 @@ const usuarioController = {
       let capa_src;
       if (capa) {
         const result = await new Promise((resolve, reject) => {
-          cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
-            if (error) reject(error);
-            else resolve(result);
-          }).end(capa[0].buffer); // Acessa o primeiro arquivo 'capa'
+          cloudinary.uploader
+            .upload_stream({ resource_type: "image" }, (error, result) => {
+              if (error) reject(error);
+              else resolve(result);
+            })
+            .end(capa[0].buffer); // Acessa o primeiro arquivo 'capa'
         });
         capa_src = result.secure_url;
       }
 
-      // criando o usuário
+      // Criação do usuário
       const usuarios = new Usuarios({
         nome,
-        apelido,
+        apelido: apelidoNormalizado,
         nascimento,
         email,
         senha: hash,
@@ -64,7 +89,7 @@ const usuarioController = {
         capa: capa_src || null,
         fontOption: "arial",
         fontSize: 1,
-        theme: "light"
+        theme: "light",
       });
 
       // salvando o usuário
@@ -72,7 +97,10 @@ const usuarioController = {
 
       const { senha: _, ...usuarioSemSenha } = usuarios.toObject();
 
-      res.status(201).json({ usuarios: usuarioSemSenha, message: "Usuário criado com sucesso!" });
+      res.status(201).json({
+        usuarios: usuarioSemSenha,
+        message: "Usuário criado com sucesso!",
+      });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Erro ao processar a requisição." });
@@ -82,7 +110,7 @@ const usuarioController = {
   getAll: async (req, res) => {
     try {
       // Exclui o campo 'senha' de todos os usuários
-      const usuarios = await Usuarios.find({}, '-senha');
+      const usuarios = await Usuarios.find({}, "-senha");
 
       res.json(usuarios);
     } catch (error) {
@@ -94,7 +122,7 @@ const usuarioController = {
   get: async (req, res) => {
     try {
       const id = req.params.id;
-      const usuario = await Usuarios.findById(id, '-senha'); // Exclui a senha
+      const usuario = await Usuarios.findById(id, "-senha"); // Exclui a senha
 
       if (!usuario) {
         res.status(404).json({ msg: "Usuário não encontrado" });
@@ -119,21 +147,21 @@ const usuarioController = {
         return;
       }
 
-      const deletedUsuario = await Usuarios.findByIdAndDelete(id).select("-senha");
+      const deletedUsuario = await Usuarios.findByIdAndDelete(id).select(
+        "-senha"
+      );
 
       res.status(200).json({
         deletedUsuario,
-        msg: "Usuário excluído com sucesso"
+        msg: "Usuário excluído com sucesso",
       });
-
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
   // atualizando o usuário passando o ID via PUT
   updateSenha: async (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
 
     const { senha } = req.body;
 
@@ -145,7 +173,7 @@ const usuarioController = {
       senha: hash,
     };
 
-    const updatedSenha = await Usuarios.findByIdAndUpdate(id, usuario)
+    const updatedSenha = await Usuarios.findByIdAndUpdate(id, usuario);
 
     if (!updatedSenha) {
       res.status(404).json({ msg: "Usuário não encontrado." });
@@ -153,21 +181,20 @@ const usuarioController = {
     }
 
     res.status(200).json({ msg: "Senha atualizada com sucesso!" });
-
   },
   // atualizando preferencias de visualização do usuário passando o ID via PUT
   updatePreferences: async (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
 
     const { fontOption, fontSize, theme } = req.body;
 
     const usuario = {
       fontOption,
       fontSize,
-      theme
+      theme,
     };
 
-    const updatedPreferences = await Usuarios.findByIdAndUpdate(id, usuario)
+    const updatedPreferences = await Usuarios.findByIdAndUpdate(id, usuario);
 
     if (!updatedPreferences) {
       res.status(404).json({ msg: "Usuário não encontrado." });
@@ -175,7 +202,6 @@ const usuarioController = {
     }
 
     res.status(200).json({ msg: "Preferências atualizadas com sucesso!" });
-
   },
   update: async (req, res) => {
     try {
@@ -189,10 +215,12 @@ const usuarioController = {
       let foto_src;
       if (foto) {
         const result = await new Promise((resolve, reject) => {
-          cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
-            if (error) reject(error);
-            else resolve(result);
-          }).end(foto[0].buffer); // Acessa o primeiro arquivo 'foto'
+          cloudinary.uploader
+            .upload_stream({ resource_type: "image" }, (error, result) => {
+              if (error) reject(error);
+              else resolve(result);
+            })
+            .end(foto[0].buffer); // Acessa o primeiro arquivo 'foto'
         });
         foto_src = result.secure_url;
       }
@@ -201,10 +229,12 @@ const usuarioController = {
       let capa_src;
       if (capa) {
         const result = await new Promise((resolve, reject) => {
-          cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
-            if (error) reject(error);
-            else resolve(result);
-          }).end(capa[0].buffer); // Acessa o primeiro arquivo 'capa'
+          cloudinary.uploader
+            .upload_stream({ resource_type: "image" }, (error, result) => {
+              if (error) reject(error);
+              else resolve(result);
+            })
+            .end(capa[0].buffer); // Acessa o primeiro arquivo 'capa'
         });
         capa_src = result.secure_url;
       }
@@ -215,24 +245,29 @@ const usuarioController = {
         nascimento,
         email,
         foto: foto_src || undefined, // Atualiza somente se houve nova foto
-        capa: capa_src || undefined,  // Atualiza somente se houve nova capa
+        capa: capa_src || undefined, // Atualiza somente se houve nova capa
       };
 
-      // Atualizando o usuário   
-      const updatedUsuario = await Usuarios.findByIdAndUpdate(id, usuario).select("-senha")
-
+      // Atualizando o usuário
+      const updatedUsuario = await Usuarios.findByIdAndUpdate(
+        id,
+        usuario
+      ).select("-senha");
 
       if (!updatedUsuario) {
         return res.status(404).json({ msg: "Usuário não encontrado." });
       }
 
-      res.status(200).json({ updatedUsuario, msg: "Usuário atualizado com sucesso!" });
+      res
+        .status(200)
+        .json({ updatedUsuario, msg: "Usuário atualizado com sucesso!" });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ msg: "Erro ao processar a atualização do usuário." });
+      res
+        .status(500)
+        .json({ msg: "Erro ao processar a atualização do usuário." });
     }
   },
 };
 
-
-module.exports = usuarioController; 
+module.exports = usuarioController;
